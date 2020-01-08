19ce8818764118cec34b6c0318c478dd674e650e
Laura Petrich <laura.petrich@gmail.com>
2020-01-08 13:49:13 -0700

    remove folders

--- sessions/plot_data.m
+++
@@ -1,7 +0,0 @@
-A = read_data('pose_data/PourMasood6D_1907251444');
-x = A(:,1);
-y = A(:,2);
-z = A(:,3);
-% fid = fopen('pose_data/PourMasood6D_1907251444');
-% % PourMasood3D_1907251440
-% % PourMasood6D_1907251444
